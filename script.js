708c42e5ad6d93b2fe69d7ade872e54a0cd2f39f
Alisson Corrales <Acorra435@west-mec.local>
2021-02-05 18:26:15 -0700

    still messig with javascript ac 2/5/2021

--- script.js
+++ script.js
@@ -23,6 +23,22 @@ voteGovern.onclick = function () {
 
     let moveToTop = (Math.random() * (screen.height - screen.width)); //takes a random number * (the height of the screen - the width of the screen)
     let moveToLeftnRight = (Math.random() * (screen.width - screen.height));//same as above but with width
+
+    //prevents button from going outside the margin of the screen
+    if(moveToLeftnRight >= 300){
+        moveToLeftnRight =- 50;
+    }
+    if(moveToLeftnRight <= -400){
+        moveToLeftnRight =+ 50
+    }
+
+    if(moveToTop >= 300){
+        moveToTop =- 100;
+    }
+    if(moveToTop <= -400){
+        moveToTop =+ 50
+    }
+
     button.style.top = moveToTop + "px";
     console.log(moveToTop + "px");
     //writes in CSS: button{margin-top: "the number that comes out of moveToTop" px}
@@ -30,13 +46,6 @@ voteGovern.onclick = function () {
     console.log(moveToLeftnRight + "px");
     //same as above but in margin-left
 
-    //prevents button from going outside the margin of the screen
-    if(moveToLeftnRight > 200){
-        moveToLeftnRight =- 100;
-    }
-    if(moveToTop > 200){
-        moveToTop =- 100;
-    }
 
     //counts the votes so the code can do a funny
     mcGovernVotes++;
@@ -46,8 +55,21 @@ voteGovern.onclick = function () {
     //the button moving part is very closely based on https://codepen.io/ardoba/pen/ZNodRK by bert bus
 }
 
-//let govToon = document.getElementById("mcgovern");
-//function(){
-//    stuff
-//}
-//govToon.addEventListener("mouseenter", handler)
+
+//switches McGovern's img to the stonk meme
+function switchToStonks(){
+    let picture = document.getElementById("mcgovern");
+    if(picture.src.match("images/mcGovern-toon.png")){
+        picture.src = "images/stonks.png"
+    }
+    picture.addEventListener("mouseover", switchToStonks);
+    return
+}
+function switchToPres(){
+    let picture = document.getElementById("mcgovern");
+    if(picture.src.match("images/stonks.png")){
+        picture.src = "images/mcGovern-toon.png"
+    }
+    picture.addEventListener("mouseleave", switchToPres);
+    return
+}
